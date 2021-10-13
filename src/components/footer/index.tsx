b99ce121d7cbb632015925dd77b34fa75c38d481
Henderson <Henderson.f.p@hotmail.com>
2021-10-13 12:13:11 -0300

    refact: footer index

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,8 +1,8 @@
-import * as S from './styles'
-import FooterSVG from '../../assets/footer.svg'
-import BottomFooter from '../../assets/subFooter.svg'
-import FooterSVGDesktop from '../../assets/footerDesktop.svg'
-import BottomFooterDesktop from '../../assets/subFooterDesktop.svg'
+import * as S 							from './styles'
+import FooterSVG 						from '../../assets/footer.svg'
+import BottomFooter 				from '../../assets/subFooter.svg'
+import FooterSVGDesktop 		from '../../assets/footerDesktop.svg'
+import BottomFooterDesktop 	from '../../assets/subFooterDesktop.svg'
 
 
 export const Footer = () => {
