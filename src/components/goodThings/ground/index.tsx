fc4f5285c27a5243c867c802cda8ae4f2265bb27
Henderson <Henderson.f.p@hotmail.com>
2021-10-13 11:51:40 -0300

    refact: goodthingsComponent was refactored

--- src/components/goodThings/ground/index.tsx
+++ src/components/goodThings/ground/index.tsx
@@ -1,1 +1,57 @@
-export {}
+import * as S 									from './styles'
+import { useState } 						from 'react'
+import { goodThings } 					from './goodthingsContent'
+import { GoodThingsCard } 			from '../../../components/goodThings/cards'
+import CircleGray 							from '../../../assets/EllipseGray.svg'
+import CircleGreen 							from '../../../assets/EllipseGreen.svg'
+
+interface GoodThingsGroundProps {
+	'onDesktopScreen': boolean,
+}
+
+const goodThingsArrayLength = Number((goodThings.length / 3).toFixed(0))
+const goodThingsInitialArray = Array(goodThingsArrayLength).fill(1, 0, 1).fill(0, 1)
+
+export const GoodThingsGround = ({onDesktopScreen}: GoodThingsGroundProps) => {
+
+	const [goodThingsIndexed, setGoodThingsIndexed] = useState<number[]>(goodThingsInitialArray)
+
+
+	const updateGoodThingsIndex = (i: number) => {
+		const array = [...goodThingsIndexed]
+		const lastIndex = array.indexOf(1)
+		array[i] = 1
+		array[lastIndex] = 0
+		setGoodThingsIndexed(array)
+	}
+
+	return (
+		<S.GoodThings>
+		<S.GoodThingsSection>
+			<h1 className='title'>GoodThings</h1>
+			<div className='div-cards'>
+				{
+					onDesktopScreen?
+					<>
+						<GoodThingsCard props={goodThings[goodThingsIndexed.indexOf(1) * 1 + 3 ]} />
+						<GoodThingsCard props={goodThings[goodThingsIndexed.indexOf(1) * 2 + 2 ]} />
+						<GoodThingsCard props={goodThings[goodThingsIndexed.indexOf(1) * 3 + 1 ]} />
+					</>
+					:
+					<GoodThingsCard props={goodThings[goodThingsIndexed.indexOf(1)]} />
+				}
+			</div>
+		</S.GoodThingsSection>
+		<div className='good-things-circles'>
+						{
+							goodThingsIndexed.map((num, i) =>
+							!!num ? 
+							<S.CirclesImg onClick={() => updateGoodThingsIndex(i)} key={i} src={CircleGreen} /> 
+							: 
+							<S.CirclesImg onClick={() => updateGoodThingsIndex(i)} key={i} src={CircleGray} />
+							)
+						}
+					</div>
+		</S.GoodThings>
+	)
+}
--- src/components/goodThings/ground/styles.ts
+++ src/components/goodThings/ground/styles.ts
@@ -1,1 +1,87 @@
 import styled from 'styled-components'
+
+export const GoodThingsSection = styled.div `
+
+	width: 347px;
+	height: 661px;
+
+	background-color: #4AC959;
+
+	padding: 0 30px;
+
+	border-radius: 10px;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: space-evenly;
+
+	.title {
+		font-size: 40px;
+		font-weight: 700;
+		text-align: end;
+
+		color: #fff;
+	}
+
+	@media (min-width: 768px) {
+		width: 1080px;
+		height: 520px;
+
+		.title {
+			position: relative;
+			top: 50px;
+			left: 50px;
+
+			text-align: start;
+		}
+		
+		.div-cards {
+			width: 1200px;
+
+			display: flex;
+			justify-content: space-evenly;
+
+			position: relative;
+			top: 120px;
+			left: 50px;
+		}
+	}
+
+`
+
+export const GoodThings = styled.div `
+	height: 710px;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: center;
+
+	.good-things-circles {
+		min-width: 30%;
+		margin-top: 30px;
+
+		display: flex;
+	}
+
+	.good-things-circles > img {
+		margin-right: 10px;
+	}
+
+	margin-top: 100px;
+
+	@media (min-width: 768px) {
+
+		margin-top: 150px;
+
+		.good-things-circles {
+			margin-top: 150px;
+			min-width: 13%;
+		}
+	}
+`
+
+
+export const CirclesImg = styled.img `
+	cursor: pointer;
+`
